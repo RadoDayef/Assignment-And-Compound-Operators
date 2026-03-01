d345f1fb19ec55067755db5ca88b3c89d6dab777
Maroooo <rado.dayef@gmail.com>
2026-03-01 06:43:01 +0200

    Full Video Code

--- bin/main.dart
+++ bin/main.dart
@@ -1,1 +1,28 @@
-void main() {}
+void main() {
+  int num = 10;
+
+  num++;
+  print(num); // Output: 11
+  num--;
+  print(num); // Output: 10
+
+  num = 10;
+  num += 5;
+  print(num); // Output: 15
+
+  num = 10;
+  num -= 3;
+  print(num); // Output: 7
+
+  num = 10;
+  num *= 2;
+  print(num); // Output: 20
+
+  num = 10;
+  num ~/= 4;
+  print(num); // Output: 2
+
+  num = 10;
+  num %= 3;
+  print(num); // Output: 1
+}
